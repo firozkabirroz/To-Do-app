938dd47b6e06f004e8a3e482ae80d4a005f56da8
firozkabirroz <firozkabirroz@gmail.com>
2021-09-11 10:48:19 +0600

    push

---
+++ middleware/user_jwt.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async function(req,res,next){
+    const token = req.header('Authorization');
+
+    if(!token){
+        return res.json.status(401).json({
+            msg:'User Not Found'
+        });
+    }
+    try{
+        await jwt.verify(token,process.env.jwtUserSecret,(err,decoded)=>{
+            if(err){
+                res.status(401).json({
+                    msg:"Token not valid"
+                });
+            }
+            else{
+                req.user = decoded.user;
+                next();
+            }
+        });
+    }
+    catch(err){
+        console.log('Something went wrong middleware '+err);
+        res.json(500).json({
+            msg: 'server error'
+        });
+
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcryptjs = require('bcryptjs');
+const user_jwt = require('../middleware/user_jwt');
+
 
 router.post('/register',async (req,res,next)=>{
-    // res.json({
-    //     msg: 'Working'
-    // });
-    // console.log(req.body);
 
     const {username,email,password}=req.body;
 
@@ -32,13 +30,24 @@ router.post('/register',async (req,res,next)=>{
         user.avatar = "https://gravater.com/avatar/?s="+size+"&d=retro";
         await user.save();
 
-        res.json({
-            success: true,
-            msg: 'user registered success',
-            user: user
-        })
+        const payload ={
+            user:{
+                id: user.id
+            }
+        }
 
+        jwt.sign(payload,process.env.jwtUserSecret,{
+            expiresIn: 360000
+        },(err,token)=>{
+            if(err) throw err;
 
+            res.status(200).json({
+                success: true,
+                token: token
+            });
+        });
+
+        
     }
     catch(err){
         console.log(err);
